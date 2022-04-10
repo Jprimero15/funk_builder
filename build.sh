9f34ec0ac2b0f538a55d7ee7c065341092ad941b
Joshua Primero <jprimero155@gmail.com>
2022-04-10 13:01:59 +0800

    More change

--- build.sh
+++ build.sh
@@ -4,9 +4,12 @@ MANIFEST="https://gitlab.com/OrangeFox/sync.git"
 OEM="xiaomi"
 DEVICE="mi439"
 DT_LINK="https://github.com/Jprimero15/recovery_device_xiaomi_olive.git"
-DT_PATH=device/${OEM}/${DEVICE}
+DT_PATH="device/${OEM}/${DEVICE}"
+RSOURCE="twrp"
+IMGTARGET="recoveryimage"
+CUSTOM_REC="Orangefox"
+OUTPUT="${CUSTOM_REC}*.zip"
 BLDR="🦊 CI-Builder:"
-#EXTRA_CMD=""
 
 apt install openssh-server -y
 apt update --fix-missing
@@ -24,7 +27,7 @@ function tg_msg() {
 tg_msg "<b>${BLDR} Setting up Build Environment</b>"
 
 
-tg_msg "<b>${BLDR} Syncing OrangeFox Sources</b>"
+tg_msg "<b>${BLDR} Syncing $CUSTOM_REC Sources</b>"
 git clone ${MANIFEST} ~/FOX && cd ~/FOX || exit
 ./orangefox_sync.sh --branch 11.0 --path ~/fox_11.0
 cd ~/fox_11.0 || exit
@@ -39,23 +42,18 @@ git clone ${DT_LINK} ${DT_PATH}
 dt_commit="$(git -C ${DT_PATH} rev-parse HEAD)"
 tg_msg "<b>${BLDR} GIT Device Tree HEAD: <code>${dt_commit}</code></b>"
 
-#echo " ===+++ Running the Extra Command... +++==="
-#$EXTRA_CMD
-
-tg_msg "<b>${BLDR} Building OrangeFox Recovery</b>"
+tg_msg "<b>${BLDR} Building $CUSTOM_REC Recovery</b>"
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export LC_ALL="C"
-lunch twrp_${DEVICE}-eng && mka recoveryimage
+lunch ${RSOURCE}_${DEVICE}-eng && mka $IMGTARGET
 
-# Upload ofox zip
-tg_msg "<b>${BLDR} Uploading OrangeFox Recovery</b>"
+# Upload zip
+tg_msg "<b>${BLDR} Uploading $CUSTOM_REC Recovery</b>"
 
 cd out/target/product/${DEVICE} || exit
 
 # Set FILENAME var
-OUTPUT="OrangeFox*.zip"
 FILENAME="$(echo ${OUTPUT})"
 
 curl -F "document=@${FILENAME}" --form-string "caption=<b>Build Target: <code>${DEVICE} Variant</code></b>
